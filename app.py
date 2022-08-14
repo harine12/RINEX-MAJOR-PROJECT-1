161e5bc9618778e0e84e608993a519b54d3dbee6
harine12 <111227359+harine12@users.noreply.github.com>
2022-08-14 19:17:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ year = st.number_input("year", min_value=1996, max_value=2060)
 mileage = st.number_input("mileage", min_value=1, max_value=200000)
 tax = st.number_input("tax in euros", min_value=0, max_value=600)
 mpg = st.number_input("miles per gallon", min_value=10, max_value=203)
-engineSize = st.number_input("Engine size in litres", min_value=1, max_value=5, step=1., format="%.2f")
+engineSize = st.number_input("Engine size in litres", min_value=1.0, max_value=5.0, step=1., format="%.2f")
 
 input_data = [[year, mileage, tax, mpg, engineSize]]
 pred = model.predict(input_data)
